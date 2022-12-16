2c8a431cdeb193bef32f1cea66d8afd5d1e352ef
MindulaHewadewage <mindufernando@gmail.com>
2022-12-16 23:30:34 +0100

    event listener

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log ('JS OK')
+// Funzione per creare una cella
+function createCell(){
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+}
+
+
+
+// Prendo gli elementi dal DOM
+const button = document.getElementById('button');
+const sectionContainer = document.getElementById('section-container');
+const grid = document.getElementById('grid'); 
+
+// struttura della griglia
+const rows = 8;
+const cols = 8;
+const totalCells = rows * cols;
+
+// Aggancio il bottone
+button.addEventListener('click', function (){
+
+    // levo il display none 
+    sectionContainer.classList.remove('d-none');
+
+    // si riportano le celle in pagina
+    for(let i = 0; i > totalCells; i++){
+
+        // creo una cella
+        createCell();
+    }
+
+
+})
